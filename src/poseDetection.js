9ad122ff5fccbb64c9ba3e4d81c3f7ce86aeacbd
Alexis Ospino Gutierrez <154022491+Apepsis@users.noreply.github.com>
2026-08-21 15:11:47 -0500

    Update poseDetection.js

--- src/poseDetection.js
+++ src/poseDetection.js
@@ -1,15 +1,92 @@
+const canvas = document.getElementById("canvas");
+
+const ctx = canvas.getContext("2d");
+
+
+canvas.width = 720;
+canvas.height = 540;
+
+
+
 const pose = new Pose({
+
 locateFile:(file)=>{
+
 return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+
 }
+
 });
+
+
 
 pose.setOptions({
+
 modelComplexity:1,
+
 smoothLandmarks:true,
-enableSegmentation:true
+
+enableSegmentation:false,
+
+minDetectionConfidence:0.5,
+
+minTrackingConfidence:0.5
+
 });
 
+
+
+
+
 pose.onResults(results=>{
-console.log("IA detectando cuerpo", results.poseLandmarks);
+
+
+ctx.clearRect(
+0,
+0,
+canvas.width,
+canvas.height
+);
+
+
+
+if(results.poseLandmarks){
+
+
+drawConnectors(
+
+ctx,
+
+results.poseLandmarks,
+
+POSE_CONNECTIONS,
+
+{
+color:"#00ff00",
+lineWidth:3
+}
+
+);
+
+
+
+drawLandmarks(
+
+ctx,
+
+results.poseLandmarks,
+
+{
+color:"#ff0000",
+radius:4
+}
+
+);
+
+
+
+}
+
+
+
 });
